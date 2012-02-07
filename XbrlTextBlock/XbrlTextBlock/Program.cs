dada98f2a60cbe9ebd13db4af151443ae3f790f3
Dustin Litorja <botchla411@yahoo.com>
2012-02-07 13:58:07 -0600

    added basic reader functionality

--- XbrlTextBlock/XbrlTextBlock/Program.cs
+++ XbrlTextBlock/XbrlTextBlock/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Xml;
 
 namespace XbrlTextBlock
 {
@@ -9,6 +10,26 @@ namespace XbrlTextBlock
     {
         static void Main(string[] args)
         {
+            XmlTextReader reader = new XmlTextReader("goog-20111231.xml");
+
+            while (reader.Read())
+            {
+                switch (reader.NodeType)
+                {
+                    case XmlNodeType.Element:
+                        Console.Write("<" + reader.Name);
+                        Console.WriteLine(">");
+                        break;
+                    case XmlNodeType.Text:
+                        Console.WriteLine(reader.Value);
+                        break;
+                    case XmlNodeType.EndElement:
+                        Console.Write("</" + reader.Name);
+                        Console.WriteLine(">");
+                        break;
+                }
+            }
+            Console.ReadLine();
         }
     }
 }
