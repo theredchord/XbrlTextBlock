6c57f8cb42ba478fef54f569a0be9c4c8b2cfd54
Dustin Litorja <botchla411@yahoo.com>
2012-02-09 15:17:43 -0600

    added regex to remove tags

--- XbrlTextBlock/XbrlTextBlock/Program.cs
+++ XbrlTextBlock/XbrlTextBlock/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Xml;
 using System.Xml.Linq;
 using System.IO;
+using System.Text.RegularExpressions;
 
 namespace XbrlTextBlock
 {
@@ -13,22 +14,34 @@ namespace XbrlTextBlock
         static void Main(string[] args)
         {
             XmlTextReader reader = new XmlTextReader("goog-20111231.xml");
+            using (FileStream fs = File.Create("XbrlTextBlock.csv"))
+            using (TextWriter writer = new StreamWriter(fs))
 
             while (reader.Read())
             {
                 if (reader.NodeType == XmlNodeType.Element && (!reader.Name.StartsWith("us-gaap")) && reader.Name.Contains("TextBlock"))
                 {
                     Console.WriteLine("Tag definition: " + reader.Name);
+                    
+                    writer.WriteLine(reader.Name);
 
                     var xbrlText = reader.ReadElementContentAsString();
+                    var textBlock = xbrlText.Replace(",", ";");
+
+                    String result = Regex.Replace(textBlock, @"<[^>]*>", String.Empty);
+                    var final = result.Replace("&#xA0;", " ");
 
                     Console.WriteLine(" ");
                     Console.WriteLine("Tag content:");
-                    Console.WriteLine("*** " + xbrlText + " ***");
+                    Console.WriteLine("*** " + final + " ***");
                     Console.WriteLine(" ");
+
+                    writer.Write(final);
+                    break;
                 }
             }
 
+            Console.WriteLine("DONE");
             Console.ReadLine();
         }
     }
